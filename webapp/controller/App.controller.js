1673b9d279b6c4f6aae860c7a19d66c85dc2d584
Liang Liang Wang <liang.liang.wang@sap.com>
2017-02-14 06:13:21 +0000

    Listen to Matched Events of Any Route

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -6,12 +6,19 @@ sap.ui.define([
 	return BaseController.extend("sap.ui.demo.nav.controller.App", {
 
 		onInit: function() {
+			jQuery.sap.log.setLevel(jQuery.sap.log.Level.INFO);
 			var oRouter = this.getRouter();
 			oRouter.attachBypassed(function(oEvent) {
 				var sHash = oEvent.getParameter("hash");
 				// do something here, i.e. send logging data to the back end for analysis
 				// telling what resource the user tried to access...
 				jQuery.sap.log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
+			});
+			oRouter.attachRouteMatched(function(oEvent) {
+				var sRouteName = oEvent.getParameter("name");
+				// do something, i.e. send usage statistics to back end
+				// in order to improve our app and the user experience (Build-Measure-Learn cycle)
+				jQuery.sap.log.info("User accessed route " + sRouteName + ", timestamp = " + new Date().getTime());
 			});
 
 		}
