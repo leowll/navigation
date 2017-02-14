6c3eebfbd4a5b338f4e569efa75272bccbb257bc
Liang Liang Wang <liang.liang.wang@sap.com>
2017-02-14 01:33:51 +0000

    Assign Multiple Targets When the button is pressed, a new page opens that contains two parts: a header part at the top and a content part. The content part displays a table of employees that can be sorted and searched.

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,18 +1,20 @@
 sap.ui.define([
 	"sap/ui/demo/nav/controller/BaseController"
-	], function(BaseController) {
+], function(BaseController) {
 	"use strict";
 	return BaseController.extend("sap.ui.demo.nav.controller.Home", {
-		onDisplayNotFound : function (oEvent) {
+		onDisplayNotFound: function(oEvent) {
 			//display the "notFound" target without changing the hash
 			this.getRouter().getTargets().display("notFound", {
-				fromTarget : "home"
+				fromTarget: "home"
 			});
 		},
-		onNavToEmployees : function (oEvent){
+		onNavToEmployees: function(oEvent) {
 			this.getRouter().navTo("employeeList");
+		},
+		onNavToEmployeeOverview: function(oEvent) {
+			this.getRouter().navTo("employeeOverview");
 		}
-
 
 	});
 
